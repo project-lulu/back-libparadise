c850e69dccfe41458baf432b06a02f7b251e9d87
Emilia Luminé <eqilia@national.shitposting.agency>
2024-09-13 15:40:48 +0200

    Import package to git
    Signed-off-by: Emilia Luminé <eqilia@national.shitposting.agency>

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const undici = require('undici');
+
+class Paradise {
+	constructor(config) {
+		this.super();
+
+		this.config = {
+			protocol: config.protocol || 'http',
+			host: config.host || 'localhost',
+			port: config.port || '40000',
+			url: `${this.config.protocol}://${this.config.host}:${this.config.port}`
+		}
+	}
+	async createUser() {
+		const res = await undici.request(`${this.config.url}/users/new`, {
+			method: 'POST'
+		});
+		const json = await res.json();
+
+		return json.payload;
+	}
+	async fetchUser(id) {
+		const res = await undici.request(`${this.config.url}/users/${id}`);
+		const json = await res.json();
+
+		return json.payload;
+	}
+	async listUsers() {
+		const res = await undici.request(`${this.config.url}/users`);
+		const json = await res.json();
+
+		return json.payload.ids;
+	}
+	async setUserData(id, payload) {
+		const res = await undici.request(`${this.config.url}/users/${id}`)
+		return 1;
+	}
+}
+
+module.exports = {
+	Paradise
+}
